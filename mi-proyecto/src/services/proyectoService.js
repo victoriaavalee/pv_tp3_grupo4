6598f1b081b3127a01072b5f35877cd1939e5be6
Jonatan Garcia <sacha.255555@gmail.com>
2026-05-21 16:03:07 -0300

    Creacion del proyecto mi-proyecto. Creacion del modulo de Servicio proyectoService.js con 10 proyectos con sus datos id, titulo, categoria y estado

---
+++ mi-proyecto/src/services/proyectoService.js
@@ -0,0 +1,13 @@
+export default proyectos = [
+  { id: 1, titulo: 'Sistema de Gestión de Exámenes', categoria: 'Educación', estado: 'Activo' },
+  { id: 2, titulo: 'Taller de Robótica', categoria: 'Educación', estado: 'Pendiente' },
+  { id: 3, titulo: 'Huerta Hidropónica', categoria: 'Medio Ambiente', estado: 'En pausa' },
+  { id: 4, titulo: 'Archivo Histórico', categoria: 'Investigación', estado: 'Completado' },
+  { id: 5, titulo: 'App de Asistencia', categoria: 'Desarrollo', estado: 'Activo' },
+  { id: 6, titulo: 'Rediseño de Portal Web', categoria: 'Desarrollo', estado: 'Activo' },
+  { id: 7, titulo: 'Plataforma de Traducción', categoria: 'Servicios', estado: 'Pendiente' },
+  { id: 8, titulo: 'Simulador Bursátil', categoria: 'Finanzas', estado: 'En pausa' },
+  { id: 9, titulo: 'Gestión de Presupuesto de Obra', categoria: 'Infraestructura', estado: 'Activo' },
+  { id: 10, titulo: 'Sistema de Gestión Logística', categoria: 'Operaciones', estado: 'Completado' }
+];
+
