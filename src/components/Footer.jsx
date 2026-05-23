475380b87ee3265b314a4ccce8371abda412c98c
paola-zoe <paolazoetoconas@gmail.com>
2026-05-22 23:32:47 -0300

    Arreglos

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer>
-      <p>&copy; 2026 - Grupo 4 - Programación Visual</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header>
-      <h1>Plataforma de Gestión de Proyectos</h1>
-    </header>
-  );
-}
-
-export default Header;
